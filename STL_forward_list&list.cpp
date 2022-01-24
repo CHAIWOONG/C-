ad868021ca65ae33b017604f01cb9bfd470971a5
CHAIWOONG <80874836+CHAIWOONG@users.noreply.github.com>
2022-01-24 18:38:46 +0900

    Update STL_forward_list&list.cpp

--- STL_forward_list&list.cpp
+++ STL_forward_list&list.cpp
@@ -13,21 +13,17 @@ int main()
 	foward_list<int> fwd_ist = { 1, 2, 3 }; // 한방향으로만 이동이 가능한 single linkled list
 
 	fwd_list.push_front(0) ; // 연결 리스트는 마지막 원소에 접근할 수 없으므로 맨 앞에 새로운 원소를 삽입한다.
-	
 
 	auto it = fwd_list.begin();
 	fwd_list.insert_after(it, 5) ; // it으로 받은 맨 처음 원소 뒤에 5 추가
 	                               // 특정 위치의 원소 삽입은 대해당 위치 앞에 원소의 next pointer를 수정해야 되므로 after추가 형식
-	
-	
+
  	fwd_list.pop_front() ; // 맨 앞에 원소를 삭제
  	
  	auto it = fwd_list.begin();
 
 	fwd_list.erase_after(it) ; // it으로 받은 맨 처음 원소 다음을 삭제
 	fwd_list.erase_after(it, fwd_list.end()) ; // 맨앞 원소 다음 부터 맨 마지막 원소까지 삭제
-	
-	
 	
 	foward_list<int> list12 = { 23,0,1,-3,34,32 };
 	list12.sort();     	      // { -3,0,1,23,32,34 }의 오름차순으로 정렬됨 (사용자 정의 타입 원소를 사용하는 경우에는 < 연산자가 정의되어 있다)
@@ -41,33 +37,35 @@ int main()
 	list3.unique([](int a, int b) {return (b-a) < 2; }); // { -1,1,5,10 } 리스트의 특정 원소가 앞 원소보다 2 이상 크지 않으면 삭제
 	
 	
-	
 	//---------------------------------------- list (double linkled list)-------------------------------------------
+	
+	// 임의의 위치에 있는 원소를 확인/ 변경 O(N)
+	// 임의의 위치에 있는 원소를 추가/ 제거 O(1)  
+	// -> 임의의 위치에서 원소를 추가하거나 임의 위치의 원소를 제거하는 연산을 많이 해야 할 경우에는 연결 리스트의 사용을 고려
 	
 	list<int> list = {1,2,3,4,5};
 
-    list.push_front(6); // {6,1,2,3,4,5}
-    list.push_back(6);  // {6,1,2,3,4,5,6} 양방향에 대한 접근이 가능
-   
-    list.pop_front(); // {1,2,3,4,5,6}
-    list.pop_back(); // {1,2,3,4,5} 양방향에 대한 접근이 가능
-   
-    //list.insert(100); 반복자에 대한 지정이 없으므로 오류 발생
-    list.insert(next(list.begin()),100);  // {1,100,2,3,4,5} 첫번째 원소 오른쪽에 100 추가
-    list.insert(list.begin(),100);   // {100,1,100,2,3,4,5} 첫번째 원소 자리에 100 추가
-   	
+	list.push_front(6); // {6,1,2,3,4,5}
+	list.push_back(6);  // {6,1,2,3,4,5,6} 양방향에 대한 접근이 가능
+
+	list.pop_front(); // {1,2,3,4,5,6}
+	list.pop_back(); // {1,2,3,4,5} 양방향에 대한 접근이 가능
 	
-	//list.insert(list.begin()+3,6); list의 경우 이렇게 하면 오류 발생
+	// insert : 매개변수로 받은 iterator 위치의 앞에 원소를 삽입. O(1)
+	// erase : 매개변수로 받은 iterator 위치의 원소를 삭제 후 원래 위치의 다음 원소의 iterator를 반환. O(1)
+
+	//-------------------------------------list.insert(100); 반복자에 대한 지정이 없으므로 오류 발생
+	list.insert(next(list.begin()),100);  // {1,100,2,3,4,5} 첫번째 원소 오른쪽에 100 추가
+	list.insert(list.begin(),100);   // {100,1,100,2,3,4,5} 첫번째 원소 자리에 100 추가
 	
-<<<<<<< HEAD
-    auto element = next(list.begin(),4); // list의 인덱스 참조는 next함수를 사용하여만 가능 (하나하나 다 접근해서 찾아가야 된다) 
+	//--------------------------------------list.insert(list.begin()+3,6); list의 경우 이렇게 하면 오류 발생
+    	auto element = next(list.begin(),4); // list의 인덱스 참조는 next함수를 사용하여만 가능 (하나하나 다 접근해서 찾아가야 된다) 
 					    // vector의 경우 next 없이 사용 가능 auto element = v1.begin()+4; 
-=======
-        auto element = next(list.begin(),4); // list의 인덱스 참조는 next함수를 사용하여만 가능 (하나하나 다 접근해서 찾아가야 된다) 
-					     // vector의 경우 next 없이 사용 가능 auto element = v1.begin()+4; 
-	//list = {100,1,100,2,3,4,5}
+					    // *element는 3
+	
+
+	//list = {100,1,100,2,3,4,5}   -------------------------------------------iterator 사용
 	auto begin = list.begin(); // 100
->>>>>>> af717b0b2dfaa479245a90c92babb8137d16388b
 	auto it = next(list.begin()); // *it == 1
     	auto it8 = next(list.begin(),-1); // *it8 == 5
 	
@@ -81,12 +79,13 @@ int main()
 	auto it2 = list.insert(it, 10); // { 100,10,1,100,2,3,4,5 } it인 1자리에 10 추가하여 그자리의 it2를 받음 (*it2 == 10, *it == 1)
 
 	it = prev(it); // *it == 10
-	it = list.erase(it); // *it == 1, 10을 지우고 그자리에 오는 1을 it로 업데이트하여 받음
+	it = list.erase(it); // *it == 1, (it가 가르키던 10을 지우고 그자리에 오는 1을 it로 업데이트하여 받음)
+	// 삭제의 경우, it는 무효화(invalidated)되기 때문에 *it, it++등으로 사용하면 UB를 발생시키며 일반적으로 it = L.erase(it)으로 사용
 	
         cout<< *element<<"\n\n"; 
 	// 3 출력
 	// list는 vector와 다르게 삽입과 삭제의 동작에서 원소를 실제로 이동할 필요가 없다
-	// 반복자의 무효화가 발생하지 않는다 (가리키던 원소가 삭제되지 않는 이상 나머지의 원소에 대해서는 그대로 사용이 가능하다)
+	// 반복자의 무효화가 삽입에서 발생하지 않는다 (가리키던 "원소가 삭제되지 않는 이상" 나머지의 원소에 대해서는 그대로 사용이 가능하다)
        	
 	
 	// --------------------------- Ranged-based for loops를 사용한 출력의 예시 -------------------------------------
@@ -94,6 +93,13 @@ int main()
         for(auto element: list)
 	    cout<< element<<" ";        // {100,1,100,2,3,4,5} 
 	
+	list<int> L = { 3, 1, 2 }; // ------------------------list는 random access iterator가 없기 때문에 std::sort 대신 L.sort()를 사용
+	L.sort();
+	for (auto& i : L) 
+		cout << i << ' '<< '\n'; // 1 2 3
+	L.sort(greater<>{});
+	for (auto& i : L) 
+		cout << i << ' '<< '\n'; // 3 2 1
+
 	return 0;
-	
 }
