9658bc3799da4b6b891dc90f5a97b0c8d530211d
CHAIWOONG <80874836+CHAIWOONG@users.noreply.github.com>
2022-02-01 14:28:52 +0900

    Update STLalgorithm_sort.cpp

--- STLalgorithm_sort.cpp
+++ STLalgorithm_sort.cpp
@@ -10,19 +10,30 @@ using namespace std;
 
 int main()
 {	
-	// ---------------------------------------------------------------- priority queue (heap) ----------------------------------------------------------------
+	// --------------------------------------------------------------------sort()--------------------------------------------------------------------------
+	// 기본은 오름차순 정렬
+	// sort(st, en, cmp = less<>{})의 인자로 구현
+	// list, stack, queue와 같이 v.begin() + i 등의 연산이 불가능한 container는 std::sort를 사용할 수 없다
 	
 	vector<int> v = { 3, 1, 4, 1, 5, 9, 2 };
 	int w[5] = { 1, 2, 3, 4, 5 };
-	string s = "jinhan814";
+	string s = "hello";
 
 	sort(v.begin(), v.end());
-	sort(w, w + 5, greater<>{});
+	//sort(v.rbegin(), v.rend()); // reverse iterator를 사용하여 ++해도 감소하므로 내림 차순으로 정렬
+	sort(w, w + 5, greater<>{}); // a > b를 리턴하는 greater<> functor를 이용하여 내림 차순으로 정렬
 	sort(s.begin(), s.end());
 
 	for (auto& i : v) cout << i << ' '; cout << '\n'; // 1 1 2 3 4 5 9
 	for (auto& i : w) cout << i << ' '; cout << '\n'; // 5 4 3 2 1
-	cout << s << '\n'; // 148ahijnn
+	cout << s << '\n'; // ehllo
+	
+	
+	// stable_sort
+	
+	// partial_sort
+	
+	// nth_element
 	
 	return 0;
 	
