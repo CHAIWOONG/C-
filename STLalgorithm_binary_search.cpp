274b84059a6a1e6524920b9cbfc461bb628bdc98
CHAIWOONG <80874836+CHAIWOONG@users.noreply.github.com>
2022-02-09 00:18:35 +0900

    Update STLalgorithm_binary_search.cpp

--- STLalgorithm_binary_search.cpp
+++ STLalgorithm_binary_search.cpp
@@ -5,34 +5,55 @@ int a[100005];
 int n;
 
 int binarysearch(int target){
-  int st = 0;
-  int en = n-1;
-  while(st <= en){
-    int mid = (st+en)/2;
-    if(a[mid] < target)
-      st = mid+1;
-    else if(a[mid] > target)
-      en = mid-1;
-    else
-      return 1;
-  }
-  return 0; // st > en일 경우 while문을 탈출
+    int st = 0;
+    int en = n-1;
+  
+    while(st <= en){
+      int mid = (st+en)/2;
+      
+      if(a[mid] < target)
+        st = mid+1;
+      else if(a[mid] > target)
+        en = mid-1;
+      else
+        return 1;
+    }
+    return 0; // st > en일 경우 while문을 탈출
 }
 
 int main(void) {
   
-  ios::sync_with_stdio(0);
-  cin.tie(0);
- 
-  cin >> n;
+      ios::sync_with_stdio(0);
+      cin.tie(0);
+
+      cin >> n;
+
+      for(int i = 0; i < n; i++) cin >> a[i];
+      sort(a, a+n);
   
-  for(int i = 0; i < n; i++) cin >> a[i];
-  sort(a, a+n);
-  int m;
-  cin >> m;
-  while(m--){
-    int t;
-    cin >> t;
-    cout << BinarySearch(t) << '\n';
-  }
+      int m;
+      cin >> m;
+  
+      while(m--){
+        int t;
+        cin >> t;
+        cout << BinarySearch(t) << '\n';
+      }
+  
+  // --------------------------------------------------------------------
+      cin >> n;
+  
+      for(int i = 0; i < n; i++) cin >> a[i];
+      sort(a,a+n);
+  
+      int m;
+      cin >> m;
+  
+      while(m--){
+        int t;
+        cin >> t;
+        cout << binary_search(a, a+n, t) << '\n';
+      }
+      
+      return 0;
 }
